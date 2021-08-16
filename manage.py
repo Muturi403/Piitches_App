1af128cc5be264a9d49e95c2fc8c6869c5429576
Muturi403 <faith.muturi@student.moringaschool.com>
2021-08-16 10:10:11 +0300

    Updated manage .py

---
+++ manage.py
@@ -0,0 +1,21 @@
+from flask_script import Manager, Server
+from app import create_app,db
+from app.models import User
+
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == "__main__":
+    manager.run()
